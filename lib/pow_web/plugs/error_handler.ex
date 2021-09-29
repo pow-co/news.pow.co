c792c0500db79ffda09c047747b84eebe5dfa722
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2021-09-29 20:55:02 +0400

    redirect to login for unauthenticated routes

--- lib/pow_web/plugs/error_handler.ex
+++ lib/pow_web/plugs/error_handler.ex
@@ -1,10 +1,19 @@
 defmodule PowWeb.Plugs.ErrorHandler do
   import Plug.Conn
+  alias PowWeb.Router.Helpers, as: Routes
+
 
   def auth_error(conn, {type, _reason}, _opts) do
     body = to_string(type)
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(401, body)
+
+    case body do
+      "unauthenticated" ->
+        conn
+          |> Phoenix.Controller.redirect(to: Routes.user_path(conn, :login))
+      _reason_type ->
+        conn
+          |> put_resp_content_type("text/plain")
+          |> send_resp(401, body)
+    end
   end
 end
